f97a1454daff72b5c97108f9482b0d02b59bb44e
Cooper <90701697+CooperWang0912@users.noreply.github.com>
2026-09-11 19:33:05 -0500

    Full Interaction Loop (TODO: Fix Blocking)

--- tests/inference_time_test.py
+++ tests/inference_time_test.py
@@ -12,40 +12,103 @@ from stt import record_once
 
 genie.load_predefined_character('thirtyseven')
 
-speech = record_once()
+from PyQt6.QtCore import QTimerEvent, Qt
+from PyQt6.QtOpenGLWidgets import QOpenGLWidget
+from PyQt6.QtWidgets import QApplication
+from OpenGL.GL import *
+from PyQt6.QtGui import QSurfaceFormat
+import Resources
 
-screenshot = pyautogui.screenshot()
+import live2d.v3 as live2d
+# import live2d.v2 as live2d
+os.environ["QSG_RHI_BACKEND"] = "opengl"
 
-screenshot = screenshot.resize((768, 768))
 
-output = BytesIO()
-screenshot.save(output, format='PNG')
-im_data = output.getvalue()
+class Win(QOpenGLWidget):
 
-dotenv.load_dotenv()
+    def __init__(self) -> None:
+        super().__init__()
+        self.setStyleSheet("background:transparent")
+        self.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground)
+        self.setWindowFlags(Qt.WindowType.FramelessWindowHint)
 
-client = genai.Client(api_key=os.getenv("APIKEY"))
+        self.resize(400, 500)
 
-start_time = time.perf_counter()
+        self.model: live2d.LAppModel | None = None
 
-interaction = client.interactions.create(
-    model="gemini-3.8-flash",
-    input=[
-        {"type": "text", "text": speech},
-        {
-            "type": "image",
-            "data": base64.b64encode(im_data).decode('utf-8'),
-            "mime_type": "image/png"
-        },
-    ],
-)
+    def initializeGL(self) -> None:
+        live2d.glInit()
+        self.model = live2d.LAppModel()
 
-output = interaction.output_text
+        if live2d.LIVE2D_VERSION == 3:
+            self.model.LoadModelJson(os.path.join("../Resources/v3/Haru/Haru.model3.json"))
+        else:
+            self.model.LoadModelJson(os.path.join("../Resources/v2/shizuku/shizuku.model.json"))
 
-genie.tts(
-    character_name='thirtyseven',
-    text=output,
-    play=True,
-)
+        self.startTimer(int(1000 / 120))
 
-genie.wait_for_playback_done()
+    def resizeGL(self, w: int, h: int) -> None:
+        self.model.Resize(w, h)
+
+    def paintGL(self) -> None:
+        live2d.clearBuffer(0.7, 0.5, 0.4, 0)
+        self.model.Update()
+
+        self.model.Draw()
+
+    def mouseMoveEvent(self, event):
+        x, y = event.globalPosition().x() - self.x(), event.globalPosition().y() - self.y()
+        self.model.Drag(x, y)
+        speech = record_once()
+
+        screenshot = pyautogui.screenshot()
+
+        screenshot = screenshot.resize((768, 768))
+
+        output = BytesIO()
+        screenshot.save(output, format='PNG')
+        im_data = output.getvalue()
+
+        dotenv.load_dotenv()
+
+        client = genai.Client(api_key=os.getenv("APIKEY"))
+
+        interaction = client.interactions.create(
+            model="gemini-3.8-flash",
+            input=[
+                {"type": "text", "text": "Keeping the response concise: " + speech},
+                {
+                    "type": "image",
+                    "data": base64.b64encode(im_data).decode('utf-8'),
+                    "mime_type": "image/png"
+                },
+            ],
+        )
+
+        output = interaction.output_text
+
+        print(output)
+
+        genie.tts(
+            character_name='thirtyseven',
+            text=output,
+            play=True,
+        )
+
+        genie.wait_for_playback_done()
+
+    def timerEvent(self, a0: QTimerEvent | None) -> None:
+        self.update()
+
+import sys
+
+live2d.init()
+
+app = QApplication(sys.argv)
+
+win = Win()
+
+win.show()
+app.exec()
+
+live2d.dispose()
